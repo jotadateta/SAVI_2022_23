151a81867f307a37b909a5d9a736e8fd0a0d0da9
jotadateta <joaopedrotomas16@gmail.com>
2022-10-18 17:15:49 +0100

    algumas atualizações tentei dar merge aos dois ficheiros mas sem sucesso

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import numpy as np
 import math
 
 
+
+face_cascade = cv2.CascadeClassifier('/home/jota/Documents/SAVI/savi_22-23/SAVI_Trabalho1/haarcascade_frontalface_default.xml')
+
+
 # Helper
 def face_confidence(face_distance, face_match_threshold=0.6):
     range = (1.0 - face_match_threshold)
@@ -37,40 +41,63 @@ class FaceRecognition:
 
             self.known_face_encodings.append(face_encoding)
             self.known_face_names.append(image)
-        print(self.known_face_names)
+        #print(self.known_face_names)
+        #print(self.known_face_encodings)
 
-    def run_recognition(self):
+    def run_recognition(self): 
         video_capture = cv2.VideoCapture(0)
 
         if not video_capture.isOpened():
             sys.exit('Video source not found...')
 
         while True:
-            ret, frame = video_capture.read()
+            _, frame = video_capture.read()
 
             # Only process every other frame of video to save time
             if self.process_current_frame:
-                # Resize frame of video to 1/4 size for faster face recognition processing
-                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+                # Resize frame of video to 1/2 size for faster face recognition processing
+                small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
 
                 # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-                rgb_small_frame = small_frame[:, :, ::-1]
+                rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+                # Convert the image from BGR color To Grayscale
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                
+                # Detect the faces
+                faces = face_cascade.detectMultiScale(gray, 1.1, 7)
 
+                # Draw the rectangle around each face
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                    # a mask is the same size as our image, but has only two pixel
+                    # values, 0 and 255 -- pixels with a value of 0 (background) are
+                    # ignored in the original image while mask pixels with a value of
+                    # 255 (foreground) are allowed to be kept
+                    #mask = np.zeros(frame.shape[:2], dtype="uint8")
+                    #cv2.rectangle(mask, (x, y), (x+w, y+h), 255, -1)
+                    #masked = cv2.bitwise_and(frame, frame, mask=mask)
+                    #big_mask = cv2.resize(masked, (0, 0), fx=2, fy=2)
+                
+                
                 # Find all the faces and face encodings in the current frame of video
                 self.face_locations = face_recognition.face_locations(rgb_small_frame)
                 self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
 
+                #print(self.face_locations)
+                #print(self.face_encodings)
+               
                 self.face_names = []
                 for face_encoding in self.face_encodings:
                     # See if the face is a match for the known face(s)
                     matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
-                    name = "Unknown"
-                    confidence = '???'
+                    name = "Desconhecido"
+                    confidence = '0%'
 
                     # Calculate the shortest distance to face
                     face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
 
                     best_match_index = np.argmin(face_distances)
+                    #print(best_match_index)
                     if matches[best_match_index]:
                         name = self.known_face_names[best_match_index]
                         confidence = face_confidence(face_distances[best_match_index])
@@ -81,11 +108,11 @@ class FaceRecognition:
 
             # Display the results
             for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
+                # Scale back up face locations since the frame we detected in was scaled to 1/ size
+                top *= 2
+                right *= 2
+                bottom *= 2
+                left *= 2
 
                 # Create the frame with the name
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -94,6 +121,7 @@ class FaceRecognition:
 
             # Display the resulting image
             cv2.imshow('Face Recognition', frame)
+            # cv2.imshow("Rectangular Mask", big_mask)
 
             # Hit 'q' on the keyboard to quit!
             if cv2.waitKey(1) == ord('q'):
